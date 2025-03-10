28df006a6fca2dc015f2f3fc94488dbc5e84a5eb
Aline Dantas <124791771+dantasaline@users.noreply.github.com>
2025-03-10 12:43:54 -0300

    Add files via upload

---
+++ aula03/ex01.php
@@ -0,0 +1,6 @@
+<?php
+    function boasvindas($nome){
+        echo "Seja bem vindo " . $nome;
+    }
+    boasvindas("Aline");
+?>
---
+++ aula03/ex02.php
@@ -0,0 +1,8 @@
+<?php
+    function diferenca($n1, $n2){
+        $diferenca = $n1 - $n2;
+        return $diferenca;
+    }
+    $vapo = diferenca(4,7);
+    echo"A diferença entre os números é ". $vapo;
+?>
