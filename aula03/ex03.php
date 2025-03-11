751f5a599fa2c623feac1d6d2b066aaf3f01c1fa
dantasaline <alinedantasdl@gmail.com>
2025-03-11 08:16:45 -0300

    .

---
+++ aula03/ex03.php
@@ -0,0 +1,13 @@
+<?php
+
+    function a($n){
+        if( $n % 2 ==0 ){
+            echo"O número $n é par";
+        } else{
+            echo"O número $n é ímpar";
+        }
+    }
+
+    a(10)
+
+?>
